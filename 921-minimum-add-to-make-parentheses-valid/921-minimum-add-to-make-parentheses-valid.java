b0074d14920d2b1820f53188a4e96812968f8bf1
sapratham <77914482+sapratham@users.noreply.github.com>
2022-02-09 22:16:06 +0530

    Time: 1 ms (75.03%), Space: 41.9 MB (10.93%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0,close = 0;
+        for(int i = 0 ; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                open++;
+            }else{
+                if(open == 0) close++;
+                else open--;
+            }
+        }
+        return open+close;
+    }
+}
