3d18ad2bf1ad3ac5413d80f3b9bb5c6eecae8fbc
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-05-15 11:39:29 +0530

    Time: 14 ms (13.89%), Space: 56.8 MB (41.00%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,39 +14,42 @@
  * }
  */
 class Solution {
-    public int dfs(TreeNode root ){
-        if(root == null){
-            return 0;
-        }
-        int left = dfs(root.left );
-        int right = dfs(root.right);
+//     public int dfs(TreeNode root ){
+//         if(root == null){
+//             return 0;
+//         }
+//         int left = dfs(root.left );
+//         int right = dfs(root.right);
         
-        return Math.max(left , right ) + 1;
-    }
+//         return Math.max(left , right ) + 1;
+//     }
     public int deepestLeavesSum(TreeNode root) {
         if(root == null) return 0;
-        int count =  dfs(root);
+       // int count =  dfs(root);
         int sum = 0;
         Queue<TreeNode> que = new ArrayDeque();
         que.add(root);
         while(que.size() != 0){
             int size  = que.size();
+            sum =0;
             while(size-- > 0){
+               
                 TreeNode rnode = que.remove();
-                
+                sum += rnode.val;
                 if(rnode.left != null){ 
                     que.add(rnode.left);
                 }
                 if(rnode.right != null){
                     que.add(rnode.right);
                 }
+                
             }
-            count--;
-                if(count == 1){
-                    while(que.size() != 0){
-                        sum += que.remove().val;
-                    }
-                }
+            // count--;
+            //     if(count == 1){
+            //         while(que.size() != 0){
+            //             sum += que.remove().val;
+            //         }
+            //     }
             
         }
         return sum;
