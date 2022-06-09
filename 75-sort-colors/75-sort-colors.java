989230b0dc738da0def281184c9653d54a9e5499
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-06-09 22:14:31 +0530

    Time: 3 ms (5.83%), Space: 43 MB (13.10%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,18 +1,15 @@
 class Solution {
     public void sortColors(int[] nums) {
-      bubbleSort(nums , nums.length);  
-    }
-    public static void bubbleSort(int[] sort_arr, int len){
-        
-        for (int i=0;i<len-1;++i){
+        int len = nums.length ; 
+          for (int i=0;i<len-1;++i){
 
             for(int j=0;j<len-i-1; ++j){
 
-                if(sort_arr[j+1]<sort_arr[j]){
+                if(nums[j+1]<nums[j]){
 
-                    int swap = sort_arr[j];
-                    sort_arr[j] = sort_arr[j+1];
-                    sort_arr[j+1] = swap;
+                    int swap = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = swap;
 
                 }
             }
