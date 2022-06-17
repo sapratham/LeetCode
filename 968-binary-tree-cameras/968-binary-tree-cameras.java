a6ae191962ddd8fa276e8f5ec1643426a5738bd0
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-06-17 12:15:04 +0530

    Time: 1 ms (76.65%), Space: 44.1 MB (18.28%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -15,31 +15,31 @@
  */
 class Solution {
     int count = 0;
-    final int C = 1;
+    final int  C = 1;
     final int M = 2;
     final int NM = 3;
-    
     public int minCameraCover(TreeNode root) {
-       int val = helper(root);
-        if(val == NM) {
+      int val =  postOrderCheck(root);
+        if(val == NM){
             count++;
         }
         return count;
     }
-    public int helper(TreeNode root){
+    public int postOrderCheck(TreeNode root){
         if(root == null){
             return M;
         }
-        int left = helper(root.left);
-        int right = helper(root.right);
         
-        if(left == NM || right == NM){
-            count++;
-            return C;
-        }else if(left == C || right == C){
-            return M;
-        }else{
-            return NM;
-        }
+       int left = postOrderCheck(root.left);
+       int right = postOrderCheck(root.right);
+        
+       if(left == M && right == M){
+           return NM;
+       }else if(left == NM || right == NM){
+           count++;
+           return C;
+       }else{
+           return M;
+       }
     }
 }
