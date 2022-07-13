50a7c17b9070989aedeecb05586cbf1d29badcce
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-07-13 22:11:05 +0530

    Time: 12 ms (42.32%), Space: 74.2 MB (52.00%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,25 +1,44 @@
 class Solution {
-    int[][] dp;
+  //  int[][] dp;
     public int maxProfit(int[] prices, int fee) {
-        dp = new int[prices.length + 1][2];
-        return memo(prices , fee , 0 , 0);
-    }
-    public int memo(int[] prices, int fee , int idx , int flag){
-        if(idx == prices.length){
-            return dp[idx][flag] = 0;
+            int[] prev = new int[2];
+    for(int i = prices.length - 1 ; i >= 0 ; i--){
+        int[] curr = new int[2];
+        for(int flag = 0 ; flag < 2 ; flag++){
+            if(i == prices.length){
+                curr[flag] = 0;
+                continue;
+            }
+            if(flag == 0){
+                int buyhere = -prices[i] + prev[1] - fee;
+                int notbuyhere = prev[0];
+                curr[flag] = Math.max(buyhere , notbuyhere);
+            }else{
+                int sellhere = prices[i] + prev[0];
+                int notsellhere = prev[1];
+                curr[flag] = Math.max(sellhere , notsellhere);
+            }   
         }
+        prev = curr;
+    }
+        return prev[0];
+    }
+//     public int memo(int[] prices, int fee , int idx , int flag){
+//         if(idx == prices.length){
+//             return dp[idx][flag] = 0;
+//         }
         
-        if(dp[idx][flag] != 0){
-            return dp[idx][flag];
-        }
-        if(flag == 0){
-         int buyhere = -prices[idx] + memo(prices ,fee , idx + 1 , 1);
-         int notbuy = memo(prices ,fee , idx + 1 , 0);
-            return dp[idx][flag] = Math.max(buyhere , notbuy);
-        }else{
-            int sellhere = prices[idx] + memo(prices ,fee , idx + 1 , 0) - fee;
-            int notsell = memo(prices ,fee ,idx + 1 , 1);
-            return dp[idx][flag] = Math.max(sellhere , notsell);
-        }
-    }
+//         if(dp[idx][flag] != 0){
+//             return dp[idx][flag];
+//         }
+//         if(flag == 0){
+//          int buyhere = -prices[idx] + memo(prices ,fee , idx + 1 , 1);
+//          int notbuy = memo(prices ,fee , idx + 1 , 0);
+//             return dp[idx][flag] = Math.max(buyhere , notbuy);
+//         }else{
+//             int sellhere = prices[idx] + memo(prices ,fee , idx + 1 , 0) - fee;
+//             int notsell = memo(prices ,fee ,idx + 1 , 1);
+//             return dp[idx][flag] = Math.max(sellhere , notsell);
+//         }
+//     }
 }
