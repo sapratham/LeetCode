6871d0040e3ad4fc0571ff8d5d3b09300ea8e903
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-07-23 22:55:45 +0530

    Time: 911 ms (16.33%), Space: 118.7 MB (77.18%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> sorted = new ArrayList<>(nums.length);
+        List<Integer> counts = new ArrayList<>(nums.length);
+        for (int i = nums.length - 1; i >= 0; --i) {
+            int index = binarySearch(sorted, nums[i], 0, sorted.size());
+            sorted.add(index, nums[i]);
+            counts.add(index);
+        }
+        Collections.reverse(counts);
+        return counts;
+    }
+    
+    private int binarySearch(List<Integer> sorted, int value, int start, int end) {
+        if (start >= end) return end;
+        int mid = start + (end - start) / 2;
+        int midVal = sorted.get(mid);
+        if (value <= midVal) return binarySearch(sorted, value, start, mid);
+        else return binarySearch(sorted, value, mid + 1, end);
+    }
+}
