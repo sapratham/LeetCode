c0b72a0ca5a5b5b563a984f657023559829f1da2
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-07-27 10:59:14 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (86.85%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,15 +14,16 @@
  * }
  */
 class Solution {
-    TreeNode prev=null;
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
-        //reverse postorder
-        if(root==null)
-            return;
+        if(root == null){
+            return ;
+        }
         flatten(root.right);
         flatten(root.left);
-        root.right=prev;
-        root.left=null;
-        prev=root;
+        root.left = null;
+        root.right = prev;
+        prev = root;
+        
     }
 }
