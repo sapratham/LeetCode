2a041eb898d63191510af1c655a8208ec4f0e456
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-08-05 22:22:50 +0530

    Time: 2 ms (43.73%), Space: 41.4 MB (76.01%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int hi = 0;                   // highest sum allowed
+        int lo = Integer.MIN_VALUE;  // lowest sum allowed
+        
+        for(int num : nums){
+            hi += num;
+            lo = Math.max(lo , num);
+        }
+        
+        while(lo < hi){
+            int mid = ( lo + hi )/2;
+            int limit = mid;
+            
+            int countParts = 1;
+            int currPartSum = 0;
+            for(int n : nums){
+                if(currPartSum + n <= limit){
+                    currPartSum += n;
+                }else{
+                    currPartSum = n;
+                    countParts++;
+                }
+            }
+            if(countParts > m){
+                lo = mid + 1;
+            }else{
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+}
