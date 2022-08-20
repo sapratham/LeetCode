473911b1af507da3ab6032251eeb8113b919576a
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-08-20 22:50:26 +0530

    Time: 154 ms (45.25%), Space: 138.2 MB (5.81%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,26 +1,56 @@
 class NumArray {
-    List<Integer> al = new ArrayList<>();
-    int totalSum=0;
+    class FenwickTree {
+        int[] arr;
+        FenwickTree(int[] nums){
+            arr = new int[nums.length + 1];
+            for(int i = 0; i < nums.length; i++){
+                this.update(i + 1, nums[i]);
+            }
+        }
+
+        private int rsb(int x){
+            return x & (-x);
+        }
+
+        public int getSum(int pos){
+            int res = 0;
+
+            while(pos > 0){
+                res += arr[pos];
+                pos = pos - rsb(pos);
+            }
+
+            return res;
+        }
+
+        public void update(int pos, int val){
+            while(pos < arr.length){
+                arr[pos] += val;
+                pos = pos + rsb(pos);
+            }
+        }
+    }
+
+    FenwickTree ft;
+    int[] oarr;
     public NumArray(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            al.add(nums[i]);
-            totalSum+=nums[i];
-        }
+        ft = new FenwickTree(nums);
+        oarr = nums;
     }
-    
+
     public void update(int index, int val) {
-        totalSum = totalSum + (val) - al.get(index);
-        al.set(index,val);
+        int delta = val - oarr[index]; 
+        ft.update(index + 1, delta);
+        oarr[index] = val;
     }
-    
+
     public int sumRange(int left, int right) {
-        int sum=totalSum;
-        for(int i=0;i<left;i++){
-                sum = sum - al.get(i);
-        }
-        for(int i=right+1;i<al.size();i++)
-            sum = sum - al.get(i);
-        return sum;
+        int lpos = left + 1;
+        int rpos = right + 1;
+
+        int suml = ft.getSum(lpos - 1);
+        int sumr = ft.getSum(rpos);
+        return sumr - suml;
     }
 }
 
