dde8c8ff36f8e603a1ec139df8ad02b225446f7f
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-09-10 15:49:18 +0530

    Time: 16 ms (18.94%), Space: 43.7 MB (25.48%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,25 +1,27 @@
 class Solution {
-    int[][][] dp;
+    int[][][] memo;
+
     public int maxProfit(int k, int[] prices) {
-        dp = new int[prices.length + 1][2][k + 1];
-        return memo(prices , 0 , 0 , k);
-    }
-       public int memo(int[] prices , int flag , int idx , int trans){
-        if(idx == prices.length || trans == 0){
-            return dp[idx][flag][trans] = 0;
-        }
-        if(dp[idx][flag][trans] != 0){
-            return dp[idx][flag][trans];
-        }
-        if(flag == 0){
-            int buyhere = -prices[idx] + memo(prices , 1 , idx + 1 , trans);
-            int notbuy = memo(prices , 0 , idx + 1 , trans);
-            return dp[idx][flag][trans] = Math.max(buyhere , notbuy);
-        }else{
-            int sellhere = prices[idx] + memo(prices , 0 , idx + 1 , trans - 1);
-            int notsell = memo(prices , 1 , idx + 1 , trans);
-            return dp[idx][flag][trans] = Math.max(sellhere , notsell);
-        }
+        memo = new int[prices.length + 1][2][k + 1];
+        
+        return memoization(prices, 0, 0, k);
     }
 
+    public int memoization(int[] prices, int i, int flag, int k) {
+        if (i == prices.length || k == 0) {
+            return 0;
+        }
+        if (memo[i][flag][k] != 0) {
+            return memo[i][flag][k];
+        }
+        if (flag == 0) {
+            int buyhere = -prices[i] + memoization(prices, i + 1, 1, k);
+            int notbuy = memoization(prices, i + 1, 0, k);
+            return memo[i][flag][k] = Math.max(buyhere, notbuy);
+        } else {
+            int sellhere = prices[i] + memoization(prices, i + 1, 0, k - 1);
+            int notsell = memoization(prices, i + 1, 1, k);
+            return memo[i][flag][k] = Math.max(sellhere, notsell);
+        }
+    }
 }
