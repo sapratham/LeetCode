476401700e7c94dfa6997e76eac6b40e432778a8
Pratham Saini <77914482+sapratham@users.noreply.github.com>
2022-09-12 21:12:01 +0530

    Time: 2 ms (68.47%), Space: 40.8 MB (77.91%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n == 0) return false;
+        int[] div = {2,3,5};
+        for(int d : div){
+            while(n >= d && n % d == 0){
+                n /= d;
+            }
+        }
+        return n == 1;
+    }
+}
